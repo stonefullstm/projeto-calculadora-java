ec86301014e609084117f7af0ef40d9defe2d438
stonefullstm <stonefull.stm@gmail.com>
2023-06-01 19:21:00 -0300

    fix: Ajuest na controller e na application

--- src/main/java/com/trybe/acc/java/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/trybe/acc/java/calculadora/controller/CalculadoraController.java
@@ -4,12 +4,10 @@ import com.trybe.acc.java.calculadora.domain.Resultado;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class CalculadoraController {
 
 
